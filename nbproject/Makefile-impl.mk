03a3a75a1b9dff069fa9884a3297a59bf894eb6f
JuliuChe <94694739+JuliuChe@users.noreply.github.com>
2023-02-24 15:10:24 +0100

    Test from MPLab

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=modbus.X
+PROJECTNAME=modBus_uC
 
 # Active Configuration
 DEFAULTCONF=default
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=modbus.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/modbus.X.production.hex
+CND_ARTIFACT_NAME_default=modBus_uC.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/modBus_uC.production.hex
